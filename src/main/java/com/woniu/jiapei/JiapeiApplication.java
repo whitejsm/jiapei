a6f0693e05c2d4b269eda6e1fa491f64102bdae5
whitejsm <841816207@qq.com>
2020-06-04 20:42:48 +0800

    first test

---
+++ src/main/java/com/woniu/jiapei/JiapeiApplication.java
@@ -0,0 +1,13 @@
+package com.woniu.jiapei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiapeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiapeiApplication.class, args);
+    }
+
+}
