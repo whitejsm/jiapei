2cb57ee5820054fcefb3806ad4b840f924794963
ym199511 <463335590@qq.com>
2020-06-06 15:51:57 +0800

    test1

--- src/main/java/com/woniu/jiapei/JiapeiApplication.java
+++ src/main/java/com/woniu/jiapei/JiapeiApplication.java
@@ -1,10 +1,12 @@
 package com.woniu.jiapei;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.woniu.jiapei.mapper")
 @EnableScheduling
 public class JiapeiApplication {
 
