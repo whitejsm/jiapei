88f447dacbcd7065f562d17e0cd762c1da225c51
ytmzz <ytmzz@icloud.com>
2020-06-12 15:56:11 +0800

    commit by ytmzz with report

---
+++ src/main/java/com/woniu/jiapei/condition/ReportController.java
@@ -0,0 +1,13 @@
+/**
+ * 报表相关查询
+ * @auther ytmzz
+ * @since 2020/06/12
+ */
+package com.woniu.jiapei.condition;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+
+}
